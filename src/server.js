db9daba2ff78e7aca6f9c7fbd8480b88ca59f529
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-12-03 18:22:13 +0900

    User Profile

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
+import { resolvers, typeDefs } from './schema';
+import { getUser } from './users/users.utils';
 require('dotenv').config();
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import logger from 'morgan';
 
-import { ApolloServer } from 'apollo-server';
-import schema from './schema';
-
-const server = new ApolloServer({
-  schema,
-});
 const PORT = process.env.PORT;
+const apollo = new ApolloServer({
+  resolvers,
+  typeDefs,
 
-server
-  .listen(PORT)
-  .then(() => console.log(`🦊 Server is running on http://localhost:${PORT}/`));
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
+  },
+});
+
+const app = express();
+app.use(logger('tiny'));
+apollo.applyMiddleware({ app });
+app.use('/static', express.static('uploads'));
+app.listen({ port: PORT }, () => {
+  console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
+});
